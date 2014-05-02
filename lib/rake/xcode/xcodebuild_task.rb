761caeeaa1153c89172b9611863fecd78f37f255
Adam Sharp <adsharp@me.com>
2014-05-02 21:29:47 +1000

    Add support for basic builds

--- lib/rake/xcode/xcodebuild_task.rb
+++ lib/rake/xcode/xcodebuild_task.rb
@@ -7,6 +7,31 @@ module Rake
 
   class XcodebuildTask < TaskLib
 
+    # The xcodebuild action to run. If not specified, xcodebuild's default
+    # will be used.
+    attr_accessor :action
+
+    # The name of the task. (Defaults to either the action name or :build if
+    # no action is specified.)
+    attr_accessor :name
+    def name
+      @name ||= (action.to_sym if action) || :build
+    end
+
+    def initialize
+      yield self if block_given?
+      define
+    end
+
+    private
+
+    def define
+      task(name) do |t|
+        sh *['xcodebuild', action].compact
+      end
+      self
+    end
+
   end
 
 end
--- test/test_rake_xcodebuild_task.rb
+++ test/test_rake_xcodebuild_task.rb
@@ -5,10 +5,26 @@ class TestRakeXcodebuildTask < Rake::TestCase
   include Rake
 
   def test_initialize
-    xct = Rake::XcodebuildTask.new do |t| end
+    xct = Rake::XcodebuildTask.new
     refute_nil xct
-    # assert_equal :build, xct.name
-    # assert Task.task_defined?(:build)
+    assert_nil xct.action
+    assert Task.task_defined?(xct.name), "expected task to be defined after #initialize"
+  end
+
+  def test_task_name_defaults
+    default_task = Rake::XcodebuildTask.new
+    assert_equal :build, default_task.name
+
+    custom_action = Rake::XcodebuildTask.new do |t|
+      t.action = 'test'
+    end
+    assert_equal :test, custom_action.name
+
+    custom_name = Rake::XcodebuildTask.new do |t|
+      t.action = :build
+      t.name = :build_debug
+    end
+    assert_equal :build_debug, custom_name.name
   end
 
 end
